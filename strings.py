987de25aa8c951c26add29496e132c1e43668d99
Fran-carvalho <113693977+Fran-carvalho@users.noreply.github.com>
2022-10-11 12:54:09 -0300

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,11 @@
+texto1 =  "Olá mundo."
+print (texto1)
+
+texto1  =  texto1 . replace ("mundo" , "pessoas") # Substitui 'mundo' por 'pessoas' no texto
+print (texto1)
+
+print (texto1.find('p')) # Localizar posição do caractere
+
+print ("Continue "  +  "a nadar!") #Concatenação
+
+
